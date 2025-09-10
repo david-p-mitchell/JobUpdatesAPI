6bf7fa8cde22c5d7282a1a76bcd9b8a4ce8d762b
David <davidmitchellfootball@googlemail.com>
2025-09-10 16:18:05 +0100

    add Job Service

---
+++ JobUpdatesAPI/Models/AddNewJobModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JobUpdatesAPI.Models;
+
+public class AddNewJobModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int MinSalaryExpectation { get; set; }
+    public int MaxSalaryExpectation { get; set; }
+    public bool HaveApplied { get; set; }
+}
