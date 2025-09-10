32bff28d28c32e42c28d157ac98ac61e357bbec7
David <davidmitchellfootball@googlemail.com>
2025-09-10 16:21:49 +0100

    add job service DI

--- JobUpdatesAPI/Program.cs
+++ JobUpdatesAPI/Program.cs
@@ -1,4 +1,6 @@
 using JobUpdatesAPI.Data;
+using JobUpdatesAPI.Interfaces;
+using JobUpdatesAPI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<JobUpdatesDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IJobService, JobService>();
 
 var app = builder.Build();
 
@@ -15,5 +18,7 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetRequiredService<JobUpdatesDbContext>();
 db.Database.Migrate();
+
+db.SaveChanges();
 
 app.Run();
