0ca00b2bc6bd06a3d3be6a6edb32f9209f7a9798
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-09-30 20:34:18 +0100

    Add CreateJobModel class to JobUpdatesAPI.Models
    Introduce the `CreateJobModel` class within the new
    `JobUpdatesAPI.Models` namespace. This class includes properties
    for `Name`, `Description`, `MinSalaryExpectation`, and
    `MaxSalaryExpectation`, all with `internal set` accessors.
    The model is intended for handling job creation data in the
    JobUpdatesAPI application.

---
+++ JobUpdatesAPI/Models/CreateJobModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JobUpdatesAPI.Models
+{
+    public class CreateJobModel
+    {
+        public string Name { get; internal set; }
+        public string Description { get; internal set; }
+        public int MinSalaryExpectation { get; internal set; }
+        public int MaxSalaryExpectation { get; internal set; }
+    }
+}
